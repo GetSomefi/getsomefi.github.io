baba717620bf29c93de8d34518727484b50c0a1f
Rönssiläppäri <peyte.com@gmail.com>
2023-05-30 13:09:16 +0300

    textgenerator_fix5

--- textgenerator/textgenerator.js
+++ textgenerator/textgenerator.js
@@ -11,7 +11,25 @@ const changeReferences =    (r,w,f) => {
     return f.replace(r,n)
 }
 
-const generate = () => {
+const shuffleArray = (array) => { //thanks to: https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+    let currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+}
+
+const generate = (suffle) => {
     data().then((data)=>{
         
         const grades = data[0];
@@ -19,8 +37,12 @@ const generate = () => {
         for (const key in grades) {
             console.log(key);
             const content = grades[key]
-            const contentMustHave = content["must_have"]
+            let contentMustHave = content["must_have"]
             document.getElementById(key).innerHTML = ""
+
+            if(suffle){
+                contentMustHave = shuffleArray(contentMustHave);
+            }
 
             for(i=0; i<contentMustHave.length; i++) {
                 const newContent = changeReferences("#", document.getElementById("topic").value,contentMustHave[i])
@@ -32,6 +54,7 @@ const generate = () => {
 generate()
 
 document.getElementById("update").addEventListener("click",generate)
+document.getElementById("suffle").addEventListener("click",() => generate(true));
 document.getElementById("topic").addEventListener("keyup",generate)
 
 const copyToClipboard = (e) => {
